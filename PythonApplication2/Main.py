4dbeeabab190438abd81686e20287d7a85b2f1e3
Jordan Kehrer <jkehrer@agi.com>
2016-03-08 02:00:12 -0500

    create initial Player class with constructor create Main with comments as outline

---
+++ PythonApplication2/Main.py
@@ -0,0 +1,6 @@
+from Player import Player
+
+#Read data from csv
+#assign data to player class
+#do linear regression with points as dependent and rank as independent variables
+#return smoothed values for each player rank
---
+++ PythonApplication2/Player.py
@@ -0,0 +1,6 @@
+class Player(object):
+    """description of class"""
+    def __init__(self, rank=0, points=0):
+       self.rank = rank
+       self.points = points
+
