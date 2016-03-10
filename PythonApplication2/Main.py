e73566f7d927e5d8f8ba8a585b39704b603cd65a
Jordan Kehrer <jkehrer@agi.com>
2016-03-10 00:33:39 -0500

    include csv reader change Player c lass constructor to take csv DictReader output as input create lists by position

--- PythonApplication2/Main.py
+++ PythonApplication2/Main.py
@@ -1,6 +1,16 @@
+import csv
 from Player import Player
 
+
+positions = ["QB", "WR"]
+
 #Read data from csv
+dataFile = open("c:\users\jk12559\desktop\sampleData.csv")
+dataDict = csv.DictReader(dataFile)
 #assign data to player class
+players = [Player(row) for row in dataDict]
+QB = [player for player in players if player.position == "QB"]
+WR = [player for player in players if player.position == "WR"]
 #do linear regression with points as dependent and rank as independent variables
+
 #return smoothed values for each player rank
--- PythonApplication2/Player.py
+++ PythonApplication2/Player.py
@@ -1,6 +1,7 @@
 class Player(object):
     """description of class"""
-    def __init__(self, rank=0, points=0):
-       self.rank = rank
-       self.points = points
+    def __init__(self, rowDict):
+       self.name = rowDict["Name"]
+       self.points = rowDict["Points"]
+       self.position = rowDict["Position"]
 
