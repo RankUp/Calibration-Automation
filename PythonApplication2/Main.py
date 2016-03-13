37afe086883f0038736bd867818556fb6da66816
Jordan Kehrer <jkehrer@agi.com>
2016-03-13 00:31:50 -0500

    add sorting players by points define rank property after sorting

--- PythonApplication2/Main.py
+++ PythonApplication2/Main.py
@@ -1,7 +1,6 @@
 import csv
 from Player import Player
 
-
 positions = ["QB", "WR"]
 
 #Read data from csv
@@ -11,6 +10,14 @@ dataDict = csv.DictReader(dataFile)
 players = [Player(row) for row in dataDict]
 QB = [player for player in players if player.position == "QB"]
 WR = [player for player in players if player.position == "WR"]
+playersGroup = {"QB": QB, "WR": WR}
+
+for group in playersGroup.itervalues():
+    group.sort(key = lambda x: x.points, reverse = True)
+    for player in group:
+        player.rank = group.index(player) + 1
+
+
 #do linear regression with points as dependent and rank as independent variables
 
 #return smoothed values for each player rank
--- PythonApplication2/Player.py
+++ PythonApplication2/Player.py
@@ -2,6 +2,7 @@ class Player(object):
     """description of class"""
     def __init__(self, rowDict):
        self.name = rowDict["Name"]
-       self.points = rowDict["Points"]
+       self.points = float(rowDict["Points"])
        self.position = rowDict["Position"]
+
 
