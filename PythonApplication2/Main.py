399be9b396484e5887e894407d6d8cffe9aedbe8
Jordan Kehrer <jkehrer@agi.com>
2016-03-13 14:28:43 -0400

    generalize the list of positions

--- PythonApplication2/Main.py
+++ PythonApplication2/Main.py
@@ -2,19 +2,23 @@ import csv
 from Player import Player
 from scipy import stats
 
-positions = ["QB", "WR"]
-
 #Read data from csv
 dataFile = open("c:\users\jk12559\desktop\sampleData.csv")
 dataDict = csv.DictReader(dataFile)
+
 #assign data to player class
 players = [Player(row) for row in dataDict]
 
+#Extract list of positions from data
+positions = set([player.position for player in players])
+
+#Group players by position
 playersGroup = {}
 for pos in positions:
     posList = [player for player in players if player.position == pos]
     playersGroup[pos] = posList
 
+#Rank players
 for group in playersGroup.itervalues():
     group.sort(key = lambda x: x.points, reverse = True)
     for player in group:
